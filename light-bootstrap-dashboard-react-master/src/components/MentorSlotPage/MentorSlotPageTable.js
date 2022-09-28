cffbc8a9e3db4d71dbceef11ae329bb5f0b9764b
= <=>
2022-09-28 09:31:14 +0700

    update

--- light-bootstrap-dashboard-react-master/src/components/MentorSlotPage/MentorSlotPageTable.js
+++ light-bootstrap-dashboard-react-master/src/components/MentorSlotPage/MentorSlotPageTable.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPosts } from "components/MentorSlotPage/MentorSlotPageTableSlice.js";
-import "components/MentorSlotPage/MentorSlotPageTable.css"
-
+import { MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
+import Table from 'react-bootstrap/Table';
 function MentorSlotPageTable() {
   const { posts, loading } = useSelector((state) => state.post); // Du lieu dang array
   const dispatch = useDispatch();
@@ -12,30 +12,30 @@ function MentorSlotPageTable() {
   }, []);
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>nameSlot</th>
-            <th>major</th>
-            <th>timeSlot</th>
-            <th>Decription</th>
-            <th>id</th>
+      <MDBTable>
+      <MDBTableHead>
+          <tr className='table-danger'>
+            <th scope='col'>id</th>
+            <th scope='col'>major</th>
+            <th scope='col'>timeSlot</th>
+            <th scope='col'>Decription</th>
+            <th scope='col'>nameSlot</th>
           </tr>
-        </thead>
-        <tbody>
+          </MDBTableHead>
+          <MDBTableBody>
         {posts.map(item => {
       return (
         <tr key={item.id}>
-          <td>{ item.nameSlot }</td>
+          <td>{ item.id }</td>
           <td>{ item.major }</td>
           <td>{ item.timeSlot }</td>
           <td>{ item.Decription }</td>
-          <td>{ item.id }</td>
+          <td>{ item.nameSlot }</td>
         </tr>
       );
     })}
-        </tbody>
-      </table>
+        </MDBTableBody>
+        </MDBTable>
     </div>
 
   );
