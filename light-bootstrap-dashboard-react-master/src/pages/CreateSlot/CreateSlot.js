bb838e6c4931fce23bd8ddec077c81ef6a1e95c1
hung1720 <hungtdqse140575@fpt.edu.vn>
2022-11-10 23:36:50 +0700

    update create schedule

--- light-bootstrap-dashboard-react-master/src/pages/CreateSlot/CreateSlot.js
+++ light-bootstrap-dashboard-react-master/src/pages/CreateSlot/CreateSlot.js
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { db } from "firebase.js";
+import { collection, addDoc } from "firebase/firestore";
+import { margin } from "@mui/system";
 
 export default function CreateSlot() {
-  return <div>CreateSlot</div>;
+  const [Major, setMajor] = useState("");
+  const [startTime, setStartTime] = useState();
+  const [endTime, setEndTime] = useState();
+  const usersCollectionRef = collection(db, "users");
+  const createShedule = async () => {
+    await addDoc(usersCollectionRef, {
+      major: Major,
+      startTime: startTime,
+      endTime: endTime,
+    });
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="Major..."
+        onChange={(event) => {
+          setMajor(event.target.value);
+        }}
+      />
+      <br />
+      <span>Start Time</span>
+      <input
+        type="datetime-local"
+        onChange={(event) => {
+          setStartTime(event.target.value);
+        }}
+      />
+      <br />
+      <span>End Time</span>
+      <input
+        type="datetime-local"
+        onChange={(event) => {
+          setEndTime(event.target.value);
+        }}
+      />
+      <br />
+
+      <button onClick={createShedule}>Create Schedule</button>
+    </div>
+  );
 }
